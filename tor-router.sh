95c1a5ea5a6686354e2edc52f57e8bb0c98d49e8
Danja Vasiliev <danja@k0a1a.net>
2016-09-28 14:38:52 -0700

    fixed formatting

--- tor-router.sh
+++ tor-router.sh
@@ -16,7 +16,7 @@
 ## invocation: tor-router.sh [start|stop|start_router|stop_router]
 
 if [[ $EUID -ne 0 ]]; then
-	echo 'You must be root' 1>&2
+  echo 'You must be root' 1>&2
   su -c "$0 $1" root  
   exit
 fi
@@ -30,49 +30,49 @@ start_me() {
   service tor restart && pidof tor >/dev/null || { echo 'Tor is not running!'; exit 1; }
   service redsocks restart && pidof redsocks >/dev/null || { echo 'Redsocks is not running!'; exit 1; }
 
-	## make REDSOCKS chain
-	iptables -t nat -N REDSOCKS
-	## exclude reserved addresses
-	iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
-	iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
-	iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
-	iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
-	iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
-	iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
-	iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
-	iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
-	## redirect anything else to port 12345 (redsocks)
-	iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
-	## exclude tor traffic from being redirected to redsocks
+  ## make REDSOCKS chain
+  iptables -t nat -N REDSOCKS
+  ## exclude reserved addresses
+  iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
+  iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+  iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
+  iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
+  iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
+  iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
+  iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
+  iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+  ## redirect anything else to port 12345 (redsocks)
+  iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+  ## exclude tor traffic from being redirected to redsocks
   iptables -t nat -A OUTPUT -p tcp -m owner \! --uid-owner $(id -u debian-tor) -j REDSOCKS
-  #	iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner user -j REDSOCKS
+  # iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner user -j REDSOCKS
 }
 
 start_router() {
   ifconfig $LOCAL_NIC || exit 1
-	start_me
-	sleep 1
-	ifconfig $LOCAL_NIC ${LOCAL_IP//.0/.1} up
-	iptables -t nat -A PREROUTING --in-interface $LOCAL_NIC -p tcp -j REDSOCKS
-	echo 1 > /proc/sys/net/ipv4/ip_forward
-	dnsmasq -i $LOCAL_NIC -F ${LOCAL_IP//.0*/.}${LOCAL_RANGE%-*},${LOCAL_IP//.0*/.}${LOCAL_RANGE##*-}
+  start_me
+  sleep 1
+  ifconfig $LOCAL_NIC ${LOCAL_IP//.0/.1} up
+  iptables -t nat -A PREROUTING --in-interface $LOCAL_NIC -p tcp -j REDSOCKS
+  echo 1 > /proc/sys/net/ipv4/ip_forward
+  dnsmasq -i $LOCAL_NIC -F ${LOCAL_IP//.0*/.}${LOCAL_RANGE%-*},${LOCAL_IP//.0*/.}${LOCAL_RANGE##*-}
 }
 
 stop_me() {
-	## find REDSOCKS rule in OUTPUT
-	REDSOCKS=$(iptables -t nat -L OUTPUT --line-numbers | awk '/REDSOCKS/ { print $1 }')
-	[ -z "$REDSOCKS" ] || for i in $REDSOCKS; do iptables -t nat -D OUTPUT $i; done
-	## find REDSOCKS rule in PREROUTING
-	REDSOCKS=$(iptables -t nat -L PREROUTING --line-numbers | awk '/REDSOCKS/ { print $1 }')
-	## if present delete it
-	[ -z "$REDSOCKS" ] || for i in $REDSOCKS; do iptables -t nat -D PREROUTING $i; done
-	## flush and delete REDSOCKS chain
-	iptables -t nat -F REDSOCKS
-	iptables -t nat -X REDSOCKS
-	echo 0 > /proc/sys/net/ipv4/ip_forward
+  ## find REDSOCKS rule in OUTPUT
+  REDSOCKS=$(iptables -t nat -L OUTPUT --line-numbers | awk '/REDSOCKS/ { print $1 }')
+  [ -z "$REDSOCKS" ] || for i in $REDSOCKS; do iptables -t nat -D OUTPUT $i; done
+  ## find REDSOCKS rule in PREROUTING
+  REDSOCKS=$(iptables -t nat -L PREROUTING --line-numbers | awk '/REDSOCKS/ { print $1 }')
+  ## if present delete it
+  [ -z "$REDSOCKS" ] || for i in $REDSOCKS; do iptables -t nat -D PREROUTING $i; done
+  ## flush and delete REDSOCKS chain
+  iptables -t nat -F REDSOCKS
+  iptables -t nat -X REDSOCKS
+  echo 0 > /proc/sys/net/ipv4/ip_forward
   [ -e /var/run/dnsmasq.pid ] &&
-	kill $(cat /var/run/dnsmasq.pid) && rm /var/run/dnsmasq.pid
-	[ -z $(ifconfig $LOCAL_NIC &>/dev/null| awk '/UP/') ] || ifconfig $LOCAL_NIC down
+  kill $(cat /var/run/dnsmasq.pid) && rm /var/run/dnsmasq.pid
+  [ -z $(ifconfig $LOCAL_NIC &>/dev/null| awk '/UP/') ] || ifconfig $LOCAL_NIC down
 }
 
 myip() {
@@ -87,27 +87,26 @@ myip() {
 }
 
 case "$1" in
-	start)
-		echo 'starting system-wide TOR redirection..'
-		start_me
-		sleep 1
-		myip
-		;;
-	start_router)
-		echo 'starting TOR router and system-wide redirection..'
-		start_router
-		sleep 1
-		myip
-		;;
-	stop)
-		echo 'stopping..'
-		stop_me
-		sleep 1
-		myip
-		;;
-	*)
-		echo 'Usage: tor-router.sh [start|start_router|stop]'
-		exit 1
-	;;
-esac 
-	
+  start)
+    echo 'starting system-wide TOR redirection..'
+    start_me
+    sleep 1
+    #myip
+  ;;
+  start_router)
+    echo 'starting TOR router and system-wide redirection..'
+    start_router
+    sleep 1
+    #myip
+  ;;
+  stop)
+    echo 'stopping..'
+    stop_me
+    sleep 1
+    #myip
+  ;;
+  *)
+    echo 'Usage: tor-router.sh [start|start_router|stop]'
+    exit 1
+  ;;
+esac
